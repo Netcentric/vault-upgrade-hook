40509f6702f073746dc5c3fc960cf2e31b281436
Oleksandr Tarasenko <oleksandr.tarasenko@netcentric.biz>
2017-10-25 14:25:56 +0200

    #24: serialize nodes with TreeMap.toString()

--- vault-upgrade-hook/src/main/java/biz/netcentric/vlt/upgrade/util/JsonNodeSerializer.java
+++ vault-upgrade-hook/src/main/java/biz/netcentric/vlt/upgrade/util/JsonNodeSerializer.java
@@ -9,9 +9,9 @@
 package biz.netcentric.vlt.upgrade.util;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
@@ -19,20 +19,18 @@ import javax.jcr.Property;
 import javax.jcr.PropertyIterator;
 import javax.jcr.RepositoryException;
 
-import com.google.gson.Gson;
-
 public class JsonNodeSerializer {
 
     private static final List<String> PROPERTY_PATTERN_TO_OMIT = Arrays.asList("jcr:.*");
 
-    private static final String NODE_NAME = "name";
+    private static final String NODE_NAME = "vault_upgrade_configuration_node_name";
 
     public String serialize(final Node node) throws RepositoryException {
 
         Map<String, Object> tree = getNodeMap(node);
         traversToMap(node, tree);
 
-        return new Gson().toJson(tree);
+        return tree.toString();
     }
 
     private void traversToMap(final Node node, final Map<String, Object> parentMap) throws RepositoryException {
@@ -46,13 +44,15 @@ public class JsonNodeSerializer {
     }
 
     private Map<String, Object> getNodeMap(final Node node) throws RepositoryException {
-        HashMap<String, Object> map = new HashMap<>();
+        Map<String, Object> map = new TreeMap<>();
         PropertyIterator properties = node.getProperties();
         while(properties.hasNext()) {
             Property property = properties.nextProperty();
             for (final String propPattern : PROPERTY_PATTERN_TO_OMIT) {
                 if (!property.getName().matches(propPattern)) {
-                    map.put(property.getName(), property.getValue());
+
+                    // For now we expect to see here only string value
+                    map.put(property.getName(), property.getString());
                 }
             }
         }
