2b95bfeb042ff7f85bac3ff9f9a6e8b7688f59c2
Andreas Haller <andreas.haller@netcentric.biz>
2020-06-24 11:07:20 +0200

    #55: make upgrade hook compatible with GroovyConsole 15

---
+++ vault-upgrade-hook/src/main/java/biz/netcentric/vlt/upgrade/handler/groovy/UpgradeHookScriptContext.java
@@ -0,0 +1,67 @@
+package biz.netcentric.vlt.upgrade.handler.groovy;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.apache.sling.api.resource.ResourceResolver;
+
+import com.icfolson.aem.groovy.console.api.context.ScriptContext;
+
+public class UpgradeHookScriptContext implements ScriptContext {
+
+	private ResourceResolver resourceResolver;
+	private String userId;
+	private String script;
+	private String data;
+	private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+	public UpgradeHookScriptContext setData(String data) {
+		this.data = data;
+		return this;
+	}
+
+	public UpgradeHookScriptContext setResourceResolver(ResourceResolver resourceResolver) {
+		this.resourceResolver = resourceResolver;
+		return this;
+	}
+
+	public UpgradeHookScriptContext setUserId(String userId) {
+		this.userId = userId;
+		return this;
+	}
+
+	public UpgradeHookScriptContext setScript(String script) {
+		this.script = script;
+		return this;
+	}
+
+	@Override
+	public String getData() {
+		return data;
+	}
+
+	@Override
+	public ByteArrayOutputStream getOutputStream() {
+		return outputStream ;
+	}
+
+	@Override
+	public PrintStream getPrintStream() {
+		return new PrintStream(getOutputStream());
+	}
+
+	@Override
+	public ResourceResolver getResourceResolver() {
+		return resourceResolver;
+	}
+
+	@Override
+	public String getScript() {
+		return script;
+	}
+
+	@Override
+	public String getUserId() {
+		return userId;
+	}
+}
